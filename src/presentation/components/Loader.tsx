ac07e9641286feb9d52ec1e6f09f4c5294f6719a
Jose Herrera <jherrera892016@gmail.com>
2024-07-15 09:21:38 -0500

    🔖  Generate Relese
    - Pending Implementation Refactor Components and Style

--- src/presentation/components/Loader.tsx
+++ src/presentation/components/Loader.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import LoaderKit from 'react-native-loader-kit';
 import {colors} from '../../theme/colors';
+import {ActivityIndicator} from 'react-native';
 
 const Loader = () => {
   return (
-    <LoaderKit
+    <ActivityIndicator
+      size={'small'}
       style={{width: 50, height: 50, alignSelf: 'center'}}
-      name={'BallPulseSync'}
       color={colors.ui.quaternary}
     />
   );
